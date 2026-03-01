f51671405e565ee7ab7b46238cb232a517d05177
pedrohenrique-almeida <pedrohenrique99690731@gmail.com>
2026-03-01 18:33:59 -0400

    Add files via upload

---
+++ api-springBoot/src/main/java/api/envio/mensagem/config/Unirest.java
@@ -0,0 +1,9 @@
+package api.envio.mensagem.config;
+
+class Unirest {
+
+    static Object post(String httpslocalhost8082messagesendTextGerencia) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+}
