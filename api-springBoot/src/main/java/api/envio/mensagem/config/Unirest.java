bbe8a7312def3f9edc85f91940beee77f2192e69
pedrohenrique-almeida <pedrohenrique99690731@gmail.com>
2026-03-02 13:52:37 -0400

    Delete api-springBoot directory

--- api-springBoot/src/main/java/api/envio/mensagem/config/Unirest.java
+++
@@ -1,9 +0,0 @@
-package api.envio.mensagem.config;
-
-class Unirest {
-
-    static Object post(String httpslocalhost8082messagesendTextGerencia) {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-
-}
