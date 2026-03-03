8223e3d4ba5cb3969bfee6d086b54d345a9a29e7
pedrohenrique-almeida <pedrohenrique99690731@gmail.com>
2026-03-02 23:51:57 -0400

    Add files via upload

---
+++ Gerenciador-Docker-main/Gerenciador-Docker-main/api-springBoot/src/main/java/api/envio/mensagem/config/DockerConfig.java
@@ -0,0 +1,52 @@
+package api.envio.mensagem.config;
+
+import com.github.dockerjava.api.DockerClient;
+import com.github.dockerjava.core.DefaultDockerClientConfig;
+import com.github.dockerjava.core.DockerClientBuilder;
+import com.github.dockerjava.zerodep.ZerodepDockerHttpClient;
+import java.time.Duration;
+import java.util.List;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "docker")
+public class DockerConfig {
+
+    private String host;
+    private List<String> blocked;
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public List<String> getBlocked() {
+        return blocked;
+    }
+
+    public void setBlocked(List<String> blocked) {
+        this.blocked = blocked;
+    }
+
+    @Bean
+    public DockerClient dockerClient() {
+        DefaultDockerClientConfig config = DefaultDockerClientConfig.createDefaultConfigBuilder()
+                .withDockerHost(host)
+                .build();
+
+        ZerodepDockerHttpClient httpClient = new ZerodepDockerHttpClient.Builder()
+                .dockerHost(config.getDockerHost())
+                .maxConnections(100)
+                .connectionTimeout(Duration.ofSeconds(30))
+                .build();
+
+        return DockerClientBuilder.getInstance(config)
+                .withDockerHttpClient(httpClient)
+                .build();
+    }
+}
