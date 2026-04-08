954bd18a94301c0b5dcec1eef0b462ec8566d5dc
pedrohenrique-almeida <pedrohenrique99690731@gmail.com>
2026-04-08 09:56:16 -0400

    Add files via upload

---
+++ api-springBoot/src/main/java/api/envio/mensagem/config/WhatsProperties.java
@@ -0,0 +1,20 @@
+package api.envio.mensagem.config;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "whatsapp")
+public class WhatsProperties {
+
+    private List<String> whitelist = new ArrayList<>();
+
+    public List<String> getWhitelist() {
+        return whitelist;
+    }
+
+    public void setWhitelist(List<String> whitelist) {
+        this.whitelist = whitelist;
+    }
+}
---
+++ api-springBoot/src/main/java/api/envio/mensagem/util/Icones.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package api.envio.mensagem.util;
+
+/**
+ *
+ * @author pedro.almeida
+ */
+public class Icones {
+
+    public static final String DOCKER = "\uD83D\uDC0B";
+    public static final String CONTAINER = "\uD83D\uDCE6";
+    public static final String BLOQUEADO = "\uD83D\uDEAB";
+}
